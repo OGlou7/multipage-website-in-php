f1771c1d0534af2103b68e3859f33fb48ab0a3da
Marie Louise Ogdoc <37371401+OGlou7@users.noreply.github.com>
2018-08-01 16:04:04 +0200

    forms

--- php/forms_marielouise.php
+++ php/forms_marielouise.php
@@ -66,6 +66,52 @@
 
 
 
+
+    //SMTP MAIL
+    use PHPMailer\PHPMailer\PHPMailer;
+    require '../vendor/autoload.php';
+    $mail = new PHPMailer;
+    $mail->isSMTP();
+    $mail->SMTPDebug = 2;
+    $mail->Host = 'smtp.gmail.com';
+    $mail->Port = 587;
+    $mail->SMTPSecure = 'tls';
+    $mail->SMTPAuth = true;
+    //Username to use for SMTP authentication - use full email address for gmail
+    $mail->Username = "username@gmail.com";
+    //Password to use for SMTP authentication
+    $mail->Password = "yourpassword";
+    //Set who the message is to be sent from
+    $mail->setFrom('from@example.com', 'First Last');
+    //Set an alternative reply-to address
+    $mail->addReplyTo('replyto@example.com', 'First Last');
+    //Set who the message is to be sent to
+    $mail->addAddress('whoto@example.com', 'John Doe');
+    //Set the subject line
+    $mail->Subject = 'PHPMailer GMail SMTP test';
+    //Read an HTML message body from an external file, convert referenced images to embedded,
+    //convert HTML into a basic plain-text alternative body
+    $mail->msgHTML(file_get_contents('contents.html'), __DIR__);
+    //Replace the plain text body with one created manually
+    $mail->AltBody = 'This is a plain-text message body';
+    //Attach an image file
+    $mail->addAttachment('images/phpmailer_mini.png');
+    //send the message, check for errors
+    if (!$mail->send()) {
+      echo "Mailer Error: " . $mail->ErrorInfo;
+    } else {
+      echo "Message sent!";
+    }
+    function save_mail($mail)
+    {
+      //You can change 'Sent Mail' to any other folder or tag
+      $path = "{imap.gmail.com:993/imap/ssl}[Gmail]/Sent Mail";
+      //Tell your server to open an IMAP connection using the same username and password as you used for SMTP
+      $imapStream = imap_open($path, $mail->Username, $mail->Password);
+      $result = imap_append($imapStream, $path, $mail->getSentMIMEMessage());
+      imap_close($imapStream);
+      return $result;
+
 ?>
 
 <!DOCTYPE html>
