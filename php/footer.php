d1ab61eaa97142e4da9d41d61a9ed8497901f12a
Marie Louise Ogdoc <37371401+OGlou7@users.noreply.github.com>
2018-08-02 09:04:31 +0200

    header footer

---
+++ php/footer.php
@@ -0,0 +1,20 @@
+
+<?php
+
+
+
+ ?>
+
+
+
+
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title></title>
+   </head>
+   <body>
+
+   </body>
+ </html>
