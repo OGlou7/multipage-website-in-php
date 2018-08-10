07b6d35ed0b2c2c270c350bf42d150e47e3e55a2
Marie Louise Ogdoc <37371401+OGlou7@users.noreply.github.com>
2018-08-10 13:23:20 +0200

    modif

--- php/about.php
+++ php/about.php
@@ -12,7 +12,7 @@
     <title>Association Belge des Paralysés</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom fonts for this template -->
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
@@ -63,8 +63,8 @@
     <!-- end footer -->
 
     <!-- Bootstrap core JavaScript -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="../vendor/jquery/jquery.min.js"></script>
+    <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
     <script src='../app.js'></script>
 
   </body>
